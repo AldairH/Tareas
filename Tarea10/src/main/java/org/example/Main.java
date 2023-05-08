c0339de843cf9cd269c4decfaa3b8b6e11731b2b
AldairH <aldairhj@outlook.com>
2023-05-07 23:36:16 -0600

    Tarea10

---
+++ Tarea10/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        JavaSwing ventanaSwing = new JavaSwing("Centigrados a Farenheid");
+    }
+}
